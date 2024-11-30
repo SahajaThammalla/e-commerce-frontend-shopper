c572bee5867849cb48871d9807efe24b71408dbc
SahajaThammalla <sahajathammalla2012@gmail.com>
2024-11-30 22:22:59 +0530

    imported setData

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, setData } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
